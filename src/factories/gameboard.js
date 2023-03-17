203b5d4d3179e118a02b6bb99e61c57ef57ce1ed
janaiscoding <jana.istrate@gmail.com>
2023-03-17 18:00:37 +0200

    Refactor placeships

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -11,7 +11,7 @@ const createBoard = (size) => {
 };
 
 //2. Gameboards should be able to place ships at specific coordinates by calling the ship factory function.
-const placeShip = (b, l, x, y, d) => {
+const placeShip2 = (b, l, x, y, d) => {
   // b = board, l = ship length, x = vertical coord, y = horizontal coord, d = direction
   while (l !== 0 && l < 6 && x + l < 11 && y + l < 11) {
     //does not go overboard
@@ -28,9 +28,38 @@ const placeShip = (b, l, x, y, d) => {
   }
   return b;
 };
+
+const placeShip = (b, l, x, y, d) => {
+  if (d === "V") {
+    for (let i = x; i < x + l; i++) {
+      if (b[i][y] === "H" || b[i][y] === "V") return;
+      else {
+        while (l !== 0 && l < 6 && x + l < 11 && y + l < 11) {
+          b[x][y] = "V";
+          x++;
+        }
+        l--;
+      }
+      return b;
+    }
+  } else if (d === "H") {
+    for (let j = y; j < y + l; j++) {
+      if (b[x][j] === "H" || b[x][j] === "V") return;
+      else {
+        while (l !== 0 && l < 6 && x + l < 11 && y + l < 11) {
+          b[x][y] = "H";
+          y++;
+        }
+        l--;
+      }
+    }
+    return b;
+  }
+
+};
 //Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
 const receiveAttack = (b, x, y, h) => {};
 //Gameboards should keep track of missed attacks so they can display them properly.
 
 //Gameboards should be able to report whether or not all of their ships have been sunk.
-export { createBoard, placeShip };
+export { createBoard, placeShip, placeShip2 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { createBoard, placeShip } from "./factories/gameboard";
-//board, length, vert coord, hor coord, direct
-let testBoard = createBoard(10);
-// placeShip(testBoard, 3, 5, 0, "H");
+const testBoard = createBoard(10);
 console.log(testBoard);
-// placeShip(testBoard, 5, 3, 0, "V");
+console.log('meows')
