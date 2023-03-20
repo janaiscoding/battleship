42ecbfa2305fa7b27a1ead638463524bfc7a759d
janaiscoding <jana.istrate@gmail.com>
2023-03-20 16:45:09 +0200

    Add player support

---
+++ src/factories/player.js
@@ -0,0 +1,11 @@
+//Create Player.
+//Players can take turns playing the game by attacking the enemy Gameboard.
+//The game is played against the computer, so make ‘computer’ players capable of making random plays. The AI does not have to be smart, but it should know whether or not a given move is legal. (i.e. it shouldn’t shoot the same coordinate twice).
+import Gameboard from "./gamboard";
+
+export default class Player {
+  constructor(name) {
+    this.name = name;
+    this.board = new Gameboard();
+  }
+}
